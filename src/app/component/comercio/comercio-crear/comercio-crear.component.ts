d72dc60e37fe728fd5c9e32fbb4aeeea8d5c2545
FabioV2006 <reynaldofabio3@gmail.com>
2025-11-26 20:28:36 -0500

    creacion de carpetas de comercio y dashboard

---
+++ src/app/component/comercio/comercio-crear/comercio-crear.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comercio-crear',
+  imports: [],
+  templateUrl: './comercio-crear.component.html',
+  styleUrl: './comercio-crear.component.css'
+})
+export class ComercioCrearComponent {
+
+}
---
+++ src/app/component/comercio/comercio-listar/comercio-listar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comercio-listar',
+  imports: [],
+  templateUrl: './comercio-listar.component.html',
+  styleUrl: './comercio-listar.component.css'
+})
+export class ComercioListarComponent {
+
+}
