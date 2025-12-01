39909ccda2f2b9f23384f6d3a08460507d5e1205
Dkey <david.azp01@gmail.com>
2025-11-30 22:06:21 -0500

    ultima2

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,23 +1,13 @@
 // Archivo: src/app/environment/environment.ts
 // Configuración para DESARROLLO LOCAL
-
 export const environment = {
     production: false,
-
-    // 1. Apuntamos al Backend de tu computadora (localhost)
-    apiUrl: 'http://localhost:8080/api',
-    loginUrl: 'http://localhost:8080/login',
-
-    // 2. Dominios permitidos (Lista blanca para enviar el Token)
-    // IMPORTANTE: Sin "http://" ni "https://"
-    allowedDomains: [
-        'localhost:8080'
-    ],
-
-    // 3. Rutas donde NO se debe enviar el token (Públicas)
+    apiUrl: 'https://proyecto-aplicacion-web.onrender.com/api',
+    loginUrl: 'https://proyecto-aplicacion-web.onrender.com/login',
+    allowedDomains: ['https://proyecto-aplicacion-web.onrender.com'],
     disallowedRoutes: [
-        'http://localhost:8080/login',
-        'http://localhost:8080/api/auth/register',
-        'http://localhost:8080/api/auth/forgot-password'
+        'https://proyecto-aplicacion-web.onrender.com/login',
+        'https://proyecto-aplicacion-web.onrender.com/api/auth/register',
+        'https://proyecto-aplicacion-web.onrender.com/api/auth/forgot-password'
     ]
 };
